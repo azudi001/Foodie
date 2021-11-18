5a72a662db977b2b55331e89e6eb9f8486560b06
YasirSherzad <ys.sherzad@gmail.com>
2021-11-18 07:48:33 +0430

    finalize TotalSave section design

--- src/home/TotalSaved.js
+++ src/home/TotalSaved.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Text from '../shared/Text';
+import Space from '../shared/Space';
+import { theme } from '../theme';
+
+const CONTAINER_HEIGHT = 180;
+
+const Border = () => (
+    <>
+        <View
+            style={{
+                position: 'absolute',
+                bottom: 1,
+                height: StyleSheet.hairlineWidth,
+                backgroundColor: theme.border,
+                width: '90%',
+            }}
+        />
+        <View
+            style={{
+                position: 'absolute',
+                bottom: 0,
+                height: 1,
+                backgroundColor: theme.border1,
+                borderBottomLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                width: '90%',
+                zIndex: -1,
+            }}
+        />
+    </>
+);
 
 const TotalSaved = ({
     params,
 }) => (
-    <View>
-        <Text>TotalSaved</Text>
+    <View style={{
+        display: 'flex',
+        height: CONTAINER_HEIGHT,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }}>
+        <Text style={{
+            fontSize: 47,
+            fontWeight: '400',
+            letterSpacing: 2,
+            textAlign: 'center',
+        }}>$2,340</Text>
+
+        <Space size={12} />
+
+        <Text
+            style={{
+                fontSize: 14,
+                color: theme.textSecondary,
+                textAlign: 'center',
+            }}
+        >Total saved this month</Text>
+
+        <Border />
     </View>
 );
 
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,13 @@
 export const theme = {
     primary: '#353746',
     secondary: '#FF8E18',
+    teriary: '#7D7F8D',
     card: '#494B5B',
     card2: '#2D2F3E',
     textPrimary: '#fff',
     textSecondary: '#7D7F8D',
-    icon: '#7D7F8D'
+    icon: '#7D7F8D',
+    border: 'rgba(125, 127, 141, .7)',
+    border1: '#2D2F3E'
 };
+
