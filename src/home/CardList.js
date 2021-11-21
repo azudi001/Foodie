04c7b4499c1fe9b07dee01fc70d87c70a4379ac8
YasirSherzad <ys.sherzad@gmail.com>
2021-11-21 04:30:04 +0430

    update cards

--- src/home/CardList.js
+++ src/home/CardList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, StyleSheet, Dimensions, Image } from 'react-native';
-import { HEADER_HEIGHT, LAYOUT_HORIZONTAL_PADDING } from '../utils';
+import { HEADER_HEIGHT, LAYOUT_HORIZONTAL_PADDING, BORDER_RADIUS } from '../utils';
 import Arrow from '../../assets/icons/arrow-right.svg';
 import { groupSavings } from '../repo/data';
 import { theme } from '../theme';
@@ -16,12 +16,13 @@ import Animated, {
     withDelay
 } from 'react-native-reanimated';
 import { snapPoint } from 'react-native-redash';
+import Space from '../shared/Space';
 
 const { height, width } = Dimensions.get('window');
 
 const LIST_HEIGHT = 200;
 const CARD_HEIGHT = 280;
-const CARD_BORDER_RADIUS = 26;
+// const CARD_BORDER_RADIUS = 26;
 
 const CARD_WIDTH = width - 80;
 // const side = (width + CARD_WIDTH + 200) / 2;
@@ -38,7 +39,6 @@ const CardList = ({
     <View style={{
         height: CARD_HEIGHT,
         width: width,
-        position: 'relative',
         marginVertical: 25,
         marginTop: 40
     }}>
@@ -59,40 +59,49 @@ const CardList = ({
 
 const Card = ({ item, index }) => {
 
-    const x = useSharedValue(0);
-    const y = useSharedValue(-height);
+    const DURATION = 250;
+    const ROTATEX = 15;
+
+    const DETAIL_SECTION_HEIGHT = 100;
+
+    const x = useSharedValue(width);
+    const y = useSharedValue(0);
     // const theta = Math.random() * 20 - 18;
-    const theta = randomNumber(-3, 3);
+    const theta = randomNumber(-4, 4);
     const rotateZ = useSharedValue(0);
-    const rotateX = useSharedValue(30);
+    const rotateX = useSharedValue(ROTATEX);
     const scale = useSharedValue(1);
 
+    const height = useSharedValue(CARD_HEIGHT);
+    const opacity = useSharedValue(0);
+
     useEffect(() => {
-        const delay = index * 250;
-        y.value = withDelay(
+        const delay = index * DURATION;
+        x.value = withDelay(
             delay,
             withTiming(0, {
-                duration: 250,
+                duration: DURATION,
                 easing: Easing.inOut(Easing.ease),
             }));
 
         rotateZ.value = withDelay(
             delay,
             withTiming(theta, {
-                duration: 250,
+                duration: DURATION,
                 easing: Easing.inOut(Easing.ease),
             }));
     }, [index, rotateZ, theta, y]);
 
 
     const onGestureEvent = useAnimatedGestureHandler({
-
         onStart: (_, ctx) => {
             ctx.x = x.value;
             ctx.y = y.value;
             scale.value = withTiming(1.1, { easing: Easing.inOut(Easing.ease) });
             rotateZ.value = withTiming(0, { easing: Easing.inOut(Easing.ease) });
             rotateX.value = withTiming(0, { easing: Easing.inOut(Easing.ease) });
+            height.value = withTiming(CARD_HEIGHT + DETAIL_SECTION_HEIGHT, { easing: Easing.inOut(Easing.ease) });
+            opacity.value = withDelay(80, withTiming(1, { easing: Easing.inOut(Easing.ease) }));
         },
         onActive: ({ translationX, translationY }, ctx) => {
             if (translationY < -90) {
@@ -115,7 +124,10 @@ const Card = ({ item, index }) => {
             //     }
             // });
             rotateZ.value = withTiming(theta, { easing: Easing.inOut(Easing.ease) });
-            rotateX.value = withTiming(30, { easing: Easing.inOut(Easing.ease) });
+            rotateX.value = withTiming(ROTATEX, { easing: Easing.inOut(Easing.ease) });
+            height.value = withTiming(CARD_HEIGHT, { easing: Easing.inOut(Easing.ease) });
+            opacity.value = withTiming(0, { duration: 100, easing: Easing.inOut(Easing.ease) });
+
         }
 
     });
@@ -123,51 +135,115 @@ const Card = ({ item, index }) => {
 
     const style = useAnimatedStyle(() => ({
         transform: [
-            { perspective: 1500 },
-            // { rotateX: `${rotateX.value}deg` },
-            { rotateZ: `${rotateZ.value}deg` },
+            { perspective: 2000 },
             { translateX: x.value },
             { translateY: y.value },
+            { rotateX: `${rotateX.value}deg` },
+            { rotateZ: `${rotateZ.value}deg` },
             { scale: scale.value }
-        ]
+        ],
+        height: height.value
+    }));
+
+    const detailSectionStyle = useAnimatedStyle(() => ({
+        opacity: opacity.value
     }));
 
 
     return (
+
         <View style={{
             ...StyleSheet.absoluteFillObject,
-
-            // justifyContent: 'center',
             alignItems: 'center',
-            backgroundColor: 'yellow'
-            // zIndex: 2
+            // overflow: 'hidden',
+            // backgroundColor: 'yellow',
+
         }} pointerEvents="box-none">
-            <PanGestureHandler on onGestureEvent={onGestureEvent}>
+            <PanGestureHandler
+                onGestureEvent={onGestureEvent}
+            >
                 <Animated.View style={[styles.cardDefaultStyle, style]}>
                     <Image
                         source={item.img}
                         style={{
-                            height: CARD_HEIGHT * .8,
+                            height: CARD_HEIGHT - 48,
                             width: '100%',
                             resizeMode: 'cover',
-                            // borderWidth: 1.5,
-                            borderRadius: CARD_BORDER_RADIUS,
+                            borderRadius: BORDER_RADIUS,
                         }}
                     />
-                    {/* <View
-                        style={{
-                            // margin: 10,
-                            height: CARD_HEIGHT / 1.3,
-                            width: CARD_WIDTH - 18,
-                            marginTop: 8,
-                            // marginLeft: 8,
-                            // marginRight: 8,
-                            // backgroundColor: 'purple',
-                            borderRadius: CARD_BORDER_RADIUS,
-                            borderWidth: 1
-                            // margin: 10
-                        }}
-                    /> */}
+                    {/* //FIXME: extract a method here */}
+                    <View style={{
+                        flexDirection: 'row',
+                        paddingVertical: 12,
+                        paddingHorizontal: 14,
+                        // backgroundColor: 'yellow'
+                    }}>
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+
+                        }}>
+                            <Text>
+                                ICON
+                            </Text>
+                            <Space horizontal size={5} />
+                            <Text>
+                                4.3
+                            </Text>
+                        </View>
+                        <Space horizontal size={8} />
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}>
+                            <Text>
+                                ICON
+                            </Text>
+                            <Space horizontal size={5} />
+                            <Text>
+                                4.3
+                            </Text>
+                        </View>
+                        <Space horizontal size={8} />
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}>
+                            <Text>
+                                CALIcon
+                            </Text>
+                            <Space horizontal size={5} />
+                            <Text>
+                                233
+                            </Text>
+                        </View>
+                        <View style={{ flex: 1 }} />
+
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            // marginTop: 10,
+                            // marginLeft: 10,
+                        }}>
+
+                            <Text style={{
+                                fontWeight: '600',
+                                fontSize: 16
+                            }}>
+                                $24.5
+                            </Text>
+                        </View>
+                    </View>
+
+                    <Animated.View style={[styles.detailSectionDefaultStyle, detailSectionStyle]}>
+                        <Text style={{
+                            fontSize: 16,
+                            fontWeight: '600'
+                        }}>Hawaiian Pizza with Mozzirilla cheese</Text>
+                        <Space size={5} />
+                        <Text>Hawaiian Pizza with Mozzirilla cheese Hawaiian Pizza with Mozzirilla cheese Hawaiian Pizza with Mozzirilla</Text>
+                    </Animated.View>
                 </Animated.View>
             </PanGestureHandler>
         </View>
@@ -176,22 +252,15 @@ const Card = ({ item, index }) => {
 
 const styles = StyleSheet.create({
     cardDefaultStyle: {
-        height: CARD_HEIGHT,
+        // height: CARD_HEIGHT,
         width: CARD_WIDTH,
         backgroundColor: '#fff',
         borderWidth: 1,
         // justifyContent: 'center',
-        alignItems: 'center',
-        padding: 12,
+        // alignItems: 'center',
+        padding: 3,
         // margin: 10,
-        borderRadius: CARD_BORDER_RADIUS,
-        // shadowColor: "#000",
-        // shadowOffset: {
-        //     width: 0,
-        //     height: 2,
-        // },
-        // shadowOpacity: 0.25,
-        // shadowRadius: 3.84,
+        borderRadius: BORDER_RADIUS + 4,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -200,6 +269,14 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.15,
         shadowRadius: 4.65,
         elevation: 5,
+    },
+    detailSectionDefaultStyle: {
+        flex: 1,
+        paddingHorizontal: 14,
+        paddingTop: 8,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        // backgroundColor: 'yellow'
     }
 });
 
